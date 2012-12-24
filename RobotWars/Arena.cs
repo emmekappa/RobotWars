4872fca5faedc164fb499046ea3cbffbcd944c7b
cantellim <cantellim@MOBTEK394.yoox.net>
2012-12-24 15:44:57 +0100

    First commit.

---
+++ RobotWars/Arena.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+
+namespace RobotWars
+{
+    public class Arena
+    {
+        private readonly int maxX;
+        private readonly int maxY;
+
+        public Arena(int maxX, int maxY)
+        {
+            robots = new List<Robot>();
+            this.maxX = maxX;
+            this.maxY = maxY;
+        }
+
+        private List<Robot> robots { get; set; }
+
+        public void AddRobot(Robot robot)
+        {
+            robots.Add(robot);
+        }
+    }
+}
