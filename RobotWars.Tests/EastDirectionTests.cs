cd6c94da8034be1250af9ffb48a7ab6109e26e68
Cantelli Michele <michele.cantelli@yoox.com>
2012-12-27 19:41:02 +0100

    Adding some directions tests.

---
+++ RobotWars.Tests/EastDirectionTests.cs
@@ -0,0 +1,35 @@
+﻿using NUnit.Framework;
+using SharpTestsEx;
+
+namespace RobotWars.Tests
+{
+    [TestFixture]
+    public class EastDirectionTests
+    {
+        private EastDirection eastDirection;
+
+        [SetUp]
+        public void SetUp()
+        {
+            eastDirection = new EastDirection();
+        }
+
+        [Test]
+        public void MoveForward_should_increase_X_coordinate_by_one()
+        {
+            var newPosition = eastDirection.MoveForward(new Coordinate(5, 5));
+
+            newPosition.X
+                       .Should().Be.EqualTo(6);
+        }
+
+        [Test]
+        public void MoveForward_should_not_change_Y_coordinate()
+        {
+            var newPosition = eastDirection.MoveForward(new Coordinate(5, 5));
+
+            newPosition.Y
+                       .Should().Be.EqualTo(5);
+        }
+    }
+}
---
+++ RobotWars.Tests/SouthDirectionTests.cs
@@ -0,0 +1,35 @@
+﻿using NUnit.Framework;
+using SharpTestsEx;
+
+namespace RobotWars.Tests
+{
+    [TestFixture]
+    public class SouthDirectionTests
+    {
+        private SouthDirection southDirection;
+
+        [SetUp]
+        public void SetUp()
+        {
+            southDirection = new SouthDirection();
+        }
+
+        [Test]
+        public void MoveForward_should_decrease_Y_coordinate_by_one()
+        {
+            var newPosition = southDirection.MoveForward(new Coordinate(5, 5));
+
+            newPosition.Y
+                       .Should().Be.EqualTo(4);
+        }
+
+        [Test]
+        public void MoveForward_should_not_change_Y_coordinate()
+        {
+            var newPosition = southDirection.MoveForward(new Coordinate(5, 5));
+
+            newPosition.X
+                       .Should().Be.EqualTo(5);
+        }
+    }
+}
